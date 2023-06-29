d40de8eaa9dee576390956145439ffdbf9483da3
adamjfox <adamjeffersonfox@gmail.com>
2023-06-29 16:17:32 -0700

    more js

--- game.js
+++ game.js
@@ -5,9 +5,9 @@ const scoreText = document.querySelector('#score');
 const progressBarFull = document.querySelector('#progressBarFull');
 
 let currentQuestion = {}
-let acceptAnswers = true
+let acceptingAnswers = true
 let score = 0
-let questionCounter = 0
+let questionsCounter = 0
 let availableQuestions = []
 
 let questions = [
@@ -18,75 +18,92 @@ let questions = [
         choice4: '17',
         answer: 2,
     },
-    { question: 'What is 2 + 2?',
+    { question: 'What is 5 + 2?',
+        choice1: '7',
+        choice2: '4',
+        choice3: '21',
+        choice4: '17',
+        answer: 1,
+    },
+    { question: 'What is 0 + 2?',
         choice1: '2',
         choice2: '4',
         choice3: '21',
         choice4: '17',
-        answer: 2,
+        answer: 1,
     },
-    { question: 'What is 2 + 2?',
+    { question: 'What is 15 + 2?',
         choice1: '2',
         choice2: '4',
         choice3: '21',
         choice4: '17',
-        answer: 2,
+        answer: 4,
     },
-    { question: 'What is 2 + 2?',
+    { question: 'What is 19 + 2?',
         choice1: '2',
         choice2: '4',
         choice3: '21',
         choice4: '17',
-        answer: 2,
-    },
-    { question: 'What is 2 + 2?',
-        choice1: '2',
-        choice2: '4',
-        choice3: '21',
-        choice4: '17',
-        answer: 2,
-    },
-    { question: 'What is 2 + 2?',
-        choice1: '2',
-        choice2: '4',
-        choice3: '21',
-        choice4: '17',
-        answer: 2,
-    },
-    { question: 'What is 2 + 2?',
-        choice1: '2',
-        choice2: '4',
-        choice3: '21',
-        choice4: '17',
-        answer: 2,
-    },
-    { question: 'What is 2 + 2?',
-        choice1: '2',
-        choice2: '4',
-        choice3: '21',
-        choice4: '17',
-        answer: 2,
-    },
-    { question: 'What is 2 + 2?',
-        choice1: '2',
-        choice2: '4',
-        choice3: '21',
-        choice4: '17',
-        answer: 2,
-    },
-    { question: 'What is 2 + 2?',
-        choice1: '2',
-        choice2: '4',
-        choice3: '21',
-        choice4: '17',
-        answer: 2,
-    },
+        answer: 3,
+    }
 ]
 
 const SCORE_POINTS = 100
 const MAX_QUESTIONS = 4
 
 startGame = () => {
-    questionCounter = 0
+    questionsCounter = 0
     score = 0
+    availableQuestions = [...questions]
+    getNewQuestion()
 }
+
+getNewQuestion = () => {
+    if(availableQuestions.length === 0 || questionsCounter > MAX_QUESTIONS) {
+        localStorage.setItem('mostRecentScore', score)
+
+        return window.location.assign('/end.html')
+    }
+
+    questionsCounter++
+    progressText.innerText = `Question ${questionsCounter} of ${MAX_QUESTIONS}`
+    progressBarFull.style.width = `${(questionsCounter/MAX_QUESTIONS) * 100}%`
+
+    const questionsIndex = Math.floor(Math.random() * availableQuestions.length)
+    currentQuestion = availableQuestions[questionsIndex]
+    question.innerText = currentQuestion.question
+
+    choices.forEach(choice => {
+        const number = choice.dataset['number']
+        choice.innerText = currentQuestion['choice' + number]
+    })
+
+    availableQuestions.splice(questionsIndex, 1)
+
+    acceptingAnswers = true
+}
+
+choices.forEach(choice => {
+    choice.addEventListener('click', e => {
+        if(!acceptingAnswers()) return
+
+        acceptingAnswers = false
+        const selectedChoice = e.target
+        const selectedAnswer = selectedChoice.dataset['number']
+        
+        let classToApply = selectedAnswer == currentQuestion ? 'correct' : 'incorrect'
+
+        if(classToApply === 'correct') {
+            incrementScore(SCORE_POINTS)
+        }
+
+        selectedChoice.parentElement.classlist.add(classToApply)
+
+        setTimeout(() => {
+            selectedChoice.parentElement.classlist.remove(classToApply)
+            getNewQuestion()
+        }, 1000)
+    })
+})
+
+// incrementScore 
