26e5fbda700190d6a940cc09e7d81142e76ff331
adamjfox <adamjeffersonfox@gmail.com>
2023-06-29 18:29:12 -0700

    back tracking

--- game.js
+++ game.js
@@ -1,6 +1,6 @@
 
 const question = document.querySelector('#question');
-const choices = Array.from(document.querySelectorALL('.choice-text'));
+const choices = Array.from(document.querySelectorAll('.choice-text'));
 const progressText = document.querySelector('#progressText');
 const scoreText = document.querySelector('#score');
 const progressBarFull = document.querySelector('#progressBarFull');
@@ -9,7 +9,7 @@ const progressBarFull = document.querySelector('#progressBarFull');
 let currentQuestion = {}
 let acceptingAnswers = true
 let score = 0
-let questionCounter = 0
+let questionsCounter = 0
 let availableQuestions = []
 
 let questions = [
@@ -53,92 +53,59 @@ let questions = [
 const SCORE_POINTS = 100
 const MAX_QUESTIONS = 4
 
-function startGame() {
-    questionCounter = 0
+startGame = () => {
+    questionsCounter = 0
     score = 0
     availableQuestions = [...questions]
     getNewQuestion()
 }
-
+console.log('getNewQuestion')
 getNewQuestion = () => {
+    if(availableQuestions.length === 0 || questionsCounter > MAX_QUESTIONS) {
+        localStorage.setItem('mostRecentScore', score)
 
-    //Keep track of the score
-    if (availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
-      localStorage.setItem('mostRecentScore', score)
-  
-      return window.location.assign('end.html')
+        return window.location.assign('/end.html')
     }
-    //questions changing to next one each click
-    questionCounter++
-    progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}` //backticks helps to contain any js expressions
-  
-  
-    //Keep track of what question in present
-    //randomizes question
+
+    questionsCounter++
+    progressText.innerText = `Question ${questionsCounter} of ${MAX_QUESTIONS}`
+    progressBarFull.style.width = `${(questionsCounter/MAX_QUESTIONS) * 100}%`
+
     const questionsIndex = Math.floor(Math.random() * availableQuestions.length)
     currentQuestion = availableQuestions[questionsIndex]
     question.innerText = currentQuestion.question
-  
-  
-  
-    //Choices
-  
+
     choices.forEach(choice => {
-      const number = choice.dataset['number']
-      choice.innerText = currentQuestion['choice' + number]
+        const number = choice.dataset['number']
+        choice.innerText = currentQuestion['choice' + number]
     })
-  
-    availableQuestions.splice(questionsIndex, 1) //removes element from an array by only 1 , then returns what you remove
-  
-    acceptingAnswers = true //keeps track of correct answers
-  }
-  
-  
-  //checking if answers are correct or incorrect
-  console.log(choices);
-  choices.forEach(choice => {
+
+    availableQuestions.splice(questionsIndex, 1)
+
+    acceptingAnswers = true
+}
+console.log(choices);
+choices.forEach(choice => {
     choice.addEventListener('click', e => {
-      if (!acceptingAnswers) return //if the answer is wrong just continue without changing anything
-      acceptingAnswers = false
-      const selectedChoice = e.target //if user clicks this add the event
-      const selectedAnswer = selectedChoice.dataset['number'] //the answer is the selectedchoice variable from the array's index number
-  
-  
-      /*if the selected answer is the correct answer from the index with the variable answer then if its true
-      then call the class from the CSS that changes the color accordingly if the selected answer is correct IF NOT then call the class that changes color for wrong answers*/
-      let classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect'
-  
-  
-  
-      if (classToApply === 'correct') {
-        incrementScore(SCORE_POINTS)
-      }
-  
-  
-  
-      selectedChoice.parentElement.classList.add(classToApply)
-  
-  
-      //setTimeOut(function,miliseconds,...)
-  
-      //remove the color by 1 second then move on to the next question
-      setTimeout(() => {
-  
-        selectedChoice.parentElement.classList.remove(classToApply)
-        getNewQuestion()
-      }, 1000)
-  
-  
+        if(!acceptingAnswers) return
+
+        acceptingAnswers = false
+        const selectedChoice = e.target
+        const selectedAnswer = selectedChoice.dataset['number']
+        console.log ('click')
+        let classToApply = selectedAnswer == currentQuestion ? 'correct' : 'incorrect'
+
+        if(classToApply === 'correct') {
+            incrementScore(SCORE_POINTS)
+        }
+
+        selectedChoice.parentElement.classlist.add(classToApply)
+
+        setTimeout(() => {
+            selectedChoice.parentElement.classlist.remove(classToApply)
+            getNewQuestion()
+        }, 1000)
     })
-  
-  
-    incrementScore = num => {
-      score += num
-      scoreText.innerText = score
-    }
-  
-  
-    //Moves to the next question
-    startGame()
-  
-  })
+})
+
+// incrementScore 
