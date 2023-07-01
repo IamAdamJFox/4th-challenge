327152529e43f19faa6dcc02e48a042997c2a362
adamjfox <adamjeffersonfox@gmail.com>
2023-07-01 02:27:25 -0700

    another fix

--- game.js
+++ game.js
@@ -2,6 +2,14 @@ const question = document.querySelector('#question');
 const choices = Array.from(document.querySelectorAll('.choice-text'));
 const progressText = document.querySelector('#progressText');
 const scoreText = document.querySelector('#score');
+const username = document.querySelector('#username')
+const saveScorebtn = document.querySelector('#saveScoreBtn')
+const finalScore = document.querySelector('#finalScore')
+const mostRecentScore = document.querySelector('#mostRecentScore')
+
+const highScores = JSON.parse(localStorage.getItem('highScores')) || []
+
+const MAX_HIGH_SCORES = 5
 
 let currentQuestion = {}
 let acceptingAnswers = true
@@ -109,3 +117,28 @@ incrementScore = num => {
 }
 
 startGame()
+
+finalScore.innerText = mostRecentScore
+username.addEventListener('keyup', () => {
+    saveScorebtn.disabled = !username.value
+})
+
+saveHighScore = e => {
+    e.preventDefault()
+
+    const score ={
+        score: mostRecentScore,
+        name: username.value
+    }
+
+    highScores.push(score)
+
+    highScores((a,b) => {})
+        return b.score -a.score
+
+    highScores.splice(5)
+
+    localStorage.setItem('highScores', JSON.stringify(highScores))
+    window.location.assign('/')
+    }
+
